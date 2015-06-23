a01b5c6494514ef3fa683b834ec5aebaeff10b44
dalepotter <dalepotter@gmail.com>
2015-06-23 14:55:23 +0100

    [324] Disabling use of plots.py script to see impact on nightly process

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 rm -rf out
 mkdir out
 ./fetch_data.sh &> fetch_data.log || exit 1
-python plots.py || exit 1
+# python plots.py || exit 1		# Commenting out to see effect of no plots script
 python make_csv.py || exit 1
 python speakers_kit.py || exit 1
 python make_html.py || exit 1
